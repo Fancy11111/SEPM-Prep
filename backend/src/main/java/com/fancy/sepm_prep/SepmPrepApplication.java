9caadeaad75909de7de98ec1d22479d7a6d5bdd5
Daniel Fenz <daniel.fenz.at@gmail.com>
2022-02-04 15:00:22 +0100

    initialized backend project

---
+++ backend/src/main/java/com/fancy/sepm_prep/SepmPrepApplication.java
@@ -0,0 +1,13 @@
+package com.fancy.sepm_prep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SepmPrepApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SepmPrepApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/com/fancy/sepm_prep/SepmPrepApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fancy.sepm_prep;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SepmPrepApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
